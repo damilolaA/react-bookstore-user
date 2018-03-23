cade7f86e84d67aad77259a004272ee152ea721e
damilolo <adiodamilola@yahoo.com>
2018-03-23 14:28:55 -0700

    fetch topSelling book and render on TopSelling component

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Header from './Header';
 import Footer from './Footer';
 import TopSelling from './TopSelling';
 
 class Index extends Component {
-
-	componentDidMount() {
-		axios.get('https://bookstoreappapi.herokuapp.com/api/v1/books')
-			.then(response => {
-				console.log(response.data);
-			})
-			.catch(err => {
-				console.log(err);
-			})
-	}
 
 	render() {
 		return (
--- src/components/TopSelling.js
+++ src/components/TopSelling.js
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const TopSelling = () => {
-	return (
-		<div className="main">
-			<div className="book-display">
-		      <div className="display-book"></div>
-		      <div className="info">
-		        <h2 className="book-title">Eloquent Javascript</h2>
-		        <h3 className="book-author">by Marijn Haverbeke</h3>
-		        <h3 className="book-price">$200</h3>
+class TopSelling extends Component {
 
-		        <form>
-		          <label htmlFor="book-amout">Amount</label>
-		          <input type="number" className="book-amount text-field" />
-		          <input className="def-button add-to-cart" type="submit" name="" value="Add to cart" />
-		        </form>
-		      </div>
+	constructor() {
+		super()
+		this.state = {
+			topSelling: ""
+		}
+	}
+
+	componentDidMount() {
+		axios.get('https://bookstoreappapi.herokuapp.com/api/v1/books/topSelling')
+			.then(response => {
+				var data = response.data;
+
+				this.setState({
+					topSelling: data
+				})
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	render() {
+
+		var topSelling = "";
+
+		if(this.state.topSelling) {
+			topSelling = this.state.topSelling;
+		}
+
+		console.log(topSelling);
+		var style = {
+			background: 'url('+ topSelling.imagePath + ')',
+			backgroundSize: 'cover',
+			backgroundPosition: 'center',
+			backgroundRepeat: 'no-repeat'
+		};
+
+		return (
+			<div className="main">
+				<div className="book-display">
+			      <div className="display-book" style={style}></div>
+			      <div className="info">
+			        <h2 className="book-title">{topSelling.title}</h2>
+			        <h3 className="book-author">{topSelling.author}</h3>
+			        <h3 className="book-price">{topSelling.price}</h3>
+
+			        <form>
+			          <label htmlFor="book-amout">Amount</label>
+			          <input type="number" className="book-amount text-field" />
+			          <input className="def-button add-to-cart" type="submit" name="" value="Add to cart" />
+			        </form>
+			      </div>
+			    </div>
 		    </div>
-	    </div>
-	);
+		);
+	}
 }
 
 export default TopSelling;
